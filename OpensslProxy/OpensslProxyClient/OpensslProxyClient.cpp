64aaefaaded70687f3c69a2f6b3edb454238d0f2
jimk1983 <jimk1983@163.com>
2018-09-05 21:50:17 +0800

    测试变更

--- OpensslProxy/OpensslProxyClient/OpensslProxyClient.cpp
+++ OpensslProxy/OpensslProxyClient/OpensslProxyClient.cpp
@@ -9,7 +9,26 @@
 
 int main()
 {
+	WSADATA    wsaData;
+	WORD	         wVersionRequested = 0;
+	int                 iError = 0;
+	SSL_CTX		*pstSSlCtx = NULL;
+	SSL				*pstSSL = NULL;
 
+
+
+
+	/*初始化winsock32环境*/
+	wVersionRequested = MAKEWORD(2, 2);
+	iError = WSAStartup(wVersionRequested, &wsaData);
+	if (iError != 0) {
+		return NULL;
+	}
+
+	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+		WSACleanup();
+		return NULL;
+	}
 
     return 0;
 }
