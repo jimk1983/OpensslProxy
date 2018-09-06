5b08c587af7ec26b066e50157bc1d5fdade7de14
jimk1983 <jimk1983@163.com>
2018-09-06 22:10:13 +0800

    基础代码，选择套件，准备调试TLS1.3

--- OpensslProxy/OpensslProxyClient/OpensslProxyClient.cpp
+++ OpensslProxy/OpensslProxyClient/OpensslProxyClient.cpp
@@ -4,24 +4,73 @@
 #include "stdafx.h"
 #include <WinSock2.h>
 #include <Windows.h>
+#include <Ws2tcpip.h>
 #include <openssl\ssl.h>
 
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "libssl.lib")
 #pragma comment(lib, "libcrypto.lib")
 
-int main()
+#define		MAXBUFSIZE		2048
+#define		IPADDRLEN		32
+
+void DisplayPeerCertInfo(SSL * ssl)
+{
+	X509	*cert;
+	char		*line;
+
+	cert = SSL_get_peer_certificate(ssl);
+	if (cert != NULL) {
+		printf("数字证书信息:\n");
+		line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+		printf("证书: %s\n", line);
+		free(line);
+		line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+		printf("颁发者: %s\n", line);
+		free(line);
+		X509_free(cert);
+	}
+	else
+		printf("无证书信息！\n");
+}
+
+int main(int argc, char *argv[])
 {
 	WSADATA		wsaData;
 	WORD		wVersionRequested = 0;
-	int			iError = 0;
-	SSL_CTX		*pstSSlCtx = NULL;
+	SSL_CTX	*pstSSlCtx = NULL;
 	SSL			*pstSSL = NULL;
+	struct sockaddr_in serveraddr = { 0 };
+	struct in_addr		 ipaddr = { 0 };
+	char			acBuf[MAXBUFSIZE] = { 0 };
+	char			acSevAddr[IPADDRLEN] = { 0 };
+	int			sockfd = 0;
+	int			iError = 0;
+	int			iError2 = 0;
+	int			argPort = 0;
+	int			iLen = 0;
+	int			iSendLen = 0;
+	int			iRecvLen = 0;
+	int			iCount = 0;
+
+	if (argc < 2)
+	{
+		printf("OpenSSL 1.1.1 Client UseAge: *.exe serveraddr serverport\n");
+		return -1;
+	}
+	else
+	{
+		strcpy_s(acSevAddr, IPADDRLEN, argv[1]);
+		argPort = atoi(argv[2]);
+		printf("OpenSSL1.1.1 info: %s:%d\n", acSevAddr, argPort);
+	}
 
 	/*初始化winsock32环境*/
 	wVersionRequested = MAKEWORD(2, 2);
 	iError = WSAStartup(wVersionRequested, &wsaData);
-	if (iError != 0) {
+	if (iError != 0) 
+	{
+		printf("WSAStartup error!\n");
 		return NULL;
 	}
 
@@ -29,7 +78,6 @@ int main()
 		WSACleanup();
 		return NULL;
 	}
-
 
 	/* SSL 库初始化*/
 	SSL_library_init();
@@ -39,7 +87,7 @@ int main()
 	SSL_load_error_strings();
 	ERR_load_BIO_strings(); 
 	
-	printf("openSSL1.1.1 info:\n");
+	printf("OpenSSL1.1.1 info:\n");
 	printf("%s\n", SSLeay_version(OPENSSL_VERSION));
 	printf("%s\n", SSLeay_version(OPENSSL_CFLAGS));
 	printf("%s\n", SSLeay_version(OPENSSL_BUILT_ON));
@@ -47,16 +95,110 @@ int main()
 	printf("%s\n", SSLeay_version(OPENSSL_DIR));
 	printf("%s\n", SSLeay_version(OPENSSL_ENGINES_DIR));
 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		printf("[SSL-Client]: Error-->socket error=%08x!\n", GetLastError());
+		goto ErrorExit;
+	}
 
-	/*pstSSlCtx = SSL_CTX_new(TLSv1_2_server_method());
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(argPort);
+	inet_pton(AF_INET, acSevAddr, &ipaddr);
+	serveraddr.sin_addr = ipaddr;
+
+	if (connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) != 0) {
+		printf("[SSL-Client]: Error-->connect error!\n");
+		goto ErrorExit;
+	}
+
+	pstSSlCtx = SSL_CTX_new(SSLv23_client_method());
 	if (NULL == pstSSlCtx)
 	{
 		printf("[SSL-Client]: Error-->SSL_CTX_new error!\n");
-		return -1;
-	}*/
+		goto ErrorExit;
+	}
 
+	// 将连接付给SSL
+	pstSSL = SSL_new(pstSSlCtx);
+	if (pstSSL == NULL)
+	{
+		printf("[SSL-Client]: Error-->connect error!\n");
+		goto ErrorExit;
+	}
 
+	SSL_set_fd(pstSSL, sockfd);
+
+	iError = SSL_connect(pstSSL);
+	if (iError == -1)
+	{
+		iError2 = SSL_get_error(pstSSL, iError);
+		const char* p1 = SSL_state_string(pstSSL);
+	}
+
+	DisplayPeerCertInfo(pstSSL);
+
+	//释放资源
+	memset(acBuf, 0, MAXBUFSIZE);
+	iLen += sprintf_s(acBuf, MAXBUFSIZE-1, "This is from Client Message, count=[%d]!", iCount);
+	
+	iSendLen = SSL_write(pstSSL, acBuf, iLen);
+	if (iSendLen < 0 )
+	{
+		iError2 = SSL_get_error(pstSSL, iError);
+		printf("[SSL-Client]: Error-->SSL write  ssl error=%d\n", iError2);
+		goto ErrorExit;
+	}
+
+	while (1)
+	{
+		memset(acBuf, 0, MAXBUFSIZE);
+		iRecvLen = SSL_read(pstSSL,  acBuf,  MAXBUFSIZE);
+		if (iRecvLen <=0)
+		{
+			iError2 = SSL_get_error(pstSSL, iRecvLen);
+			printf("[SSL-Client]: Error-->SSL_read  error=%d\n", iError2);
+			break;
+		}
+		else
+		{
+			printf("[SSL-Client]: SSL_read Content=%s\n", acBuf);
+		}
+
+		memset(acBuf, 0, MAXBUFSIZE);
+		iCount++;
+		iLen = 0;
+		iLen += sprintf_s(acBuf, MAXBUFSIZE - 1, "This is from Client Message, count=[%d]!", iCount);
+		iSendLen = SSL_write(pstSSL, acBuf,  iLen);
+		if (iSendLen < 0)
+		{
+			iError2 = SSL_get_error(pstSSL, iSendLen);
+			printf("[SSL-Client]: Error-->SSL write  error=%d\n", iError2);
+			break;
+		}
+	}
+	
 	system("pause");
-    return 0;
+
+ErrorExit:
+	if (sockfd > 0 )
+	{
+		closesocket(sockfd);
+	}
+
+	if (NULL != pstSSL)
+	{
+		SSL_free(pstSSL);
+		pstSSL = NULL;
+	}
+
+	if (NULL != pstSSlCtx )
+	{
+		SSL_CTX_free(pstSSlCtx);
+		pstSSlCtx = NULL;
+	}
+
+	WSACleanup();
+	return -1;
 }
 
--- OpensslProxy/OpensslProxyServer/OpensslProxyServer.cpp
+++ OpensslProxy/OpensslProxyServer/OpensslProxyServer.cpp
@@ -4,57 +4,210 @@
 #include "stdafx.h"
 #include <WinSock2.h>
 #include <Windows.h>
+#include <Ws2tcpip.h>
 #include <openssl\ssl.h>
+#include <openssl/err.h>  
 
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "libssl.lib")
 #pragma comment(lib, "libcrypto.lib")
 
+#define		LISTNNUM			10
+#define		MAXBUFSIZE		2048
+#define		IPADDRLEN			32
 
 
-int main()
+int main(int argc, char *argv[])
 {
-	WSADATA    wsaData;
+	WSADATA     wsaData;
 	WORD	         wVersionRequested = 0;
-	int                 iError			= 0;
 	SSL_CTX		*pstSSlCtx	= NULL;
 	SSL				*pstSSL		= NULL;
-
-
-
+	struct  sockaddr_in  serveraddr = {0};
+	struct  sockaddr_in  clientaddr  = {0};
+	socklen_t					socklen=0;
+	struct  in_addr		    ipaddr = { 0 };
+	char			acBuf[MAXBUFSIZE] = { 0 };
+	char			acSevAddr[IPADDRLEN] = { 0 };
+	char			acCliAddr[IPADDRLEN] = { 0 };
+	int			iError = 0;
+	int			iError2 = 0;
+	int			argPort = 0;
+	int			iLen = 0;
+	int			iSendLen = 0;
+	int			iRecvLen = 0;
+	int			iCount = 0;
+	int			sockfd = 0;
+	int			new_fd = 0;
 
 	/*初始化winsock32环境*/
 	wVersionRequested = MAKEWORD(2, 2);
 	iError = WSAStartup(wVersionRequested, &wsaData);
 	if (iError != 0) {
-		return NULL;
+		return -1;
 	}
 
 	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
 		WSACleanup();
-		return NULL;
+		return -1;
 	}
 
-	/* SSL 库初始化*/
-	//SSL_library_init();
-	SSLeay_add_ssl_algorithms();
-	//OpenSSL_add_all_algorithms();
-	//SSL_load_error_strings();
-	//ERR_load_BIO_strings();
-
-	pstSSlCtx = SSL_CTX_new(TLSv1_2_server_method());
-	if (NULL == pstSSlCtx)
+	if (argc < 2)
 	{
-		printf("[SSL-Client]: Error-->SSL_CTX_new error!\n");
+		printf("****OpenSSL 1.1.1 Server UseAge: *.exe Localaddr  port*****\n");
+		return -1;
+	}
+	else
+	{
+		strcpy_s(acSevAddr, IPADDRLEN, argv[1]);
+		argPort = atoi(argv[2]);
+		printf("OpenSSL1.1.1 info: %s:%d\n", acSevAddr, argPort);
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		printf("[SSL-Server]: Error-->socket error=%08x!\n", GetLastError());
 		goto ErrorExit;
 	}
 
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(argPort);
+	inet_pton(AF_INET, acSevAddr, &ipaddr);
+	serveraddr.sin_addr = ipaddr;
+	socklen = sizeof(serveraddr);
 
-	return 0;
+	if ( bind(sockfd, (struct sockaddr *)&serveraddr, socklen) < 0 )
+	{
+		printf("[SSL-Server]: Error-->socket bind error=%08x!\n", GetLastError());
+		goto ErrorExit;
+	}
+
+	if ( listen(sockfd, LISTNNUM)  < 0 ) 
+	{
+		printf("[SSL-Server]: Error-->socket listen error=%08x!\n", GetLastError());
+		goto ErrorExit;
+	}
+
+	/* SSL 库初始化*/
+	SSL_library_init();
+	OPENSSL_malloc_init();
+	SSLeay_add_ssl_algorithms();
+	OpenSSL_add_all_algorithms();
+	SSL_load_error_strings();
+	ERR_load_BIO_strings();
+
+	printf("OpenSSL1.1.1 info:\n");
+	printf("%s\n", SSLeay_version(OPENSSL_VERSION));
+	printf("%s\n", SSLeay_version(OPENSSL_CFLAGS));
+	printf("%s\n", SSLeay_version(OPENSSL_BUILT_ON));
+	printf("%s\n", SSLeay_version(OPENSSL_PLATFORM));
+	printf("%s\n", SSLeay_version(OPENSSL_DIR));
+	printf("%s\n", SSLeay_version(OPENSSL_ENGINES_DIR));
+
+	pstSSlCtx = SSL_CTX_new(SSLv23_server_method());
+	if (NULL == pstSSlCtx)
+	{
+		printf("[SSL-Server]: Error-->SSL_CTX_new error!\n");
+		goto ErrorExit;
+	}
+
+	if (SSL_CTX_set_cipher_list(pstSSlCtx, "TLS_AES_256_GCM_SHA384") <= 0)
+	{
+		ERR_print_errors_fp(stdout);
+		goto ErrorExit;
+	}
+
+	if (SSL_CTX_use_certificate_file(pstSSlCtx, argv[4], SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stdout);
+		goto ErrorExit;
+	}
+	if (SSL_CTX_use_PrivateKey_file(pstSSlCtx, argv[5], SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stdout);
+		goto ErrorExit;
+	}
+	if (!SSL_CTX_check_private_key(pstSSlCtx)) {
+		ERR_print_errors_fp(stdout);
+		goto ErrorExit;
+	}
+
+	while (1) 
+	{
+		iLen = sizeof(struct sockaddr);
+
+		if ((new_fd = accept(sockfd, (struct sockaddr *)&clientaddr, &iLen)) == -1) 
+		{
+			printf("[SSL-Server]: Error-->socket listen error=%08x!\n", GetLastError());
+			break;
+		}
+		else
+		{
+			memset(acCliAddr,0, IPADDRLEN); 
+			inet_ntop(AF_INET, &clientaddr.sin_addr, acCliAddr, IPADDRLEN);
+			printf("server: got connection from %s, port %d, socket %d \n", acCliAddr, ntohs(clientaddr.sin_port), new_fd);
+		}
+		
+		pstSSL = SSL_new(pstSSlCtx);
+		if (NULL == pstSSL)
+		{
+			ERR_print_errors_fp(stdout);
+			break;
+		}
+
+		SSL_set_fd(pstSSL, new_fd);
+		iError = SSL_accept(pstSSL);
+		if ( iError == -1)
+		{
+			ERR_print_errors_fp(stdout);
+			break;
+		}
+
+		/*单次仅处理一个客户端的报文*/
+		while (1)
+		{
+			memset(acBuf, 0, MAXBUFSIZE);
+			iRecvLen = SSL_read(pstSSL, acBuf, MAXBUFSIZE);
+			if (iRecvLen <= 0)
+			{
+				iError2 = SSL_get_error(pstSSL, iRecvLen);
+				printf("[SSL-Server]: Error-->SSL_read  error=%d\n", iError2);
+				break;
+			}
+			else
+			{
+				printf("[SSL-Server]: SSL_read Content=%s\n", acBuf);
+			}
+
+			memset(acBuf, 0, MAXBUFSIZE);
+			iCount++;
+			iLen = 0;
+			iLen += sprintf_s(acBuf, MAXBUFSIZE - 1, "Server BackMessage, ServerHello, count=[%d]!", iCount);
+			iSendLen = SSL_write(pstSSL, acBuf, iLen);
+			if (iSendLen < 0)
+			{
+				iError2 = SSL_get_error(pstSSL, iSendLen);
+				printf("[SSL-Server]: Error-->SSL write  error=%d\n", iError2);
+				break;
+			}
+		}
+		/*先释放上一次的SSL*/
+		if (NULL != pstSSL)
+		{
+			SSL_free(pstSSL);
+			pstSSL = NULL;
+		}
+	}
 
 ErrorExit:
-	WSACleanup();
+	if (sockfd > 0)
+	{
+		closesocket(sockfd);
+	}
 
+	if ( new_fd > 0 )
+	{
+		closesocket(new_fd);
+	}
 	if (NULL != pstSSlCtx)
 	{
 		SSL_CTX_free(pstSSlCtx);
@@ -67,6 +220,7 @@ ErrorExit:
 		pstSSL = NULL;
 	}
 
+	WSACleanup();
     return -1;
 }
 
